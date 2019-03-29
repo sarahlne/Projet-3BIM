38e50d69c359c982b5c39e7846a1fa6066f539b2
sarahlne <48209473+sarahlne@users.noreply.github.com>
2019-03-29 10:06:03 +0100

    Update test.py récupération des abstract
    Ajout de la méthode pour récupérer les abstracts et mise a jour de la grande boucle, elle affiche: id, date, titre, abstract et citations des articles résultants d'une recherche antérieur à 2016 et ayant un abstract

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from Bio import Entrez
 
-def search(query):
+def search(query): #Method use to launch a research on pubmed
     Entrez.email = 'sarah-lne@outlook.com'
     handle = Entrez.esearch(db='pubmed', 
                             sort='relevance', 
@@ -10,13 +10,13 @@ def search(query):
     results = Entrez.read(handle)
     return results
 
-def article(ident):
+def article(ident): #Method to get informations about a given article
 	Entrez.email = 'sarah-lne@outlook.com'
 	handle = Entrez.esummary(db = 'pubmed', id = ident)
 	records = Entrez.read(handle)
 	return records
 
-def citation(pmid):
+def citation(pmid): #Method to get articles who cite the given article
 	Entrez.email = 'sarah-lne@outlook.com'
 	handle = Entrez.elink(dbfrom="pubmed", db="pmc",
 						LinkName="pubmed_pmc_refs", id=pmid)
@@ -27,19 +27,37 @@ def citation(pmid):
 	else: 
 		pubmed_ids = [link["Id"] for link in result[0]["LinkSetDb"][0]["Link"]]
 		return pubmed_ids 
+	
+def recup_abstract(pmids):  #Method to get the abstract of a given article
+	Entrez.email = 'sarah-lne@outlook.com'
+	handle = Entrez.efetch(db="pubmed", id=','.join(map(str, pmids)),
+	                       rettype="xml", retmode="text")
+	records = Entrez.read(handle)
+	abstracts = [pubmed_article['MedlineCitation']['Article']['Abstract']['AbstractText'][0]
+	             for pubmed_article in records['PubmedArticle']]
+
+
+	abstract_dict = dict(zip(pmids, abstracts))
+	if(abstract_dict == {}):
+		return "No abstract"
+	else: 
+		return abstract_dict
 
 #print(citation(6387394))
-print(search("baby without arms")['IdList'])
+#print(search("baby without arms")['IdList'])
 #print(citation("810668"))
 #print(article(30496090))
 #print(search("30496090"))
-"""print(search("baby without arms"))
-print(article("30496090")[0]['PubDate'])"""
+#print(search("baby without arms"))
+#print(article("30496090")[0]['PubDate'])
 
-"""for i in search("baby without arms")['IdList']:
-	if(article(str(i))[0]['PubDate'] < "2016"):
+for i in search("directed evolution")['IdList']:
+	if(article(str(i))[0]['PubDate'] < "2016" and article(str(i))[0]['HasAbstract']==1):
+		print(i)
 		print(article(str(i))[0]['PubDate'])
-		print(citation(i))"""
+		print(article(str(i))[0]['Title'])
+		print(recup_abstract([i]))
+		print(citation(i))
 
 """def info(query):
 	Entrez.email = 'sarah-lne@outlook.com'
